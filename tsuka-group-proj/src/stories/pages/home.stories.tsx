5139edf5e4bcb2158692c7335e3e8878bcc83c94
João Gustavo Camilo Júnior <joaogcamilojr@gmail.com>
2023-04-04 08:00:05 -0300

    feat: add initital storybook config

---
+++ tsuka-group-proj/src/stories/pages/home.stories.tsx
@@ -0,0 +1,9 @@
+import { Meta, StoryObj } from "@storybook/react";
+import Home from "../../pages/index";
+
+export default {
+  title: "Pages/Home",
+  component: Home,
+} as Meta;
+
+export const HomePage: StoryObj = {};
