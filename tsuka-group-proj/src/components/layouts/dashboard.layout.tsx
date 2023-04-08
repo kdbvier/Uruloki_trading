917f0142a38fcf74bd404c829c75ab4438d93b98
João Gustavo Camilo Júnior <joaogcamilojr@gmail.com>
2023-04-08 11:57:57 -0300

    fix: bg color

--- tsuka-group-proj/src/components/layouts/dashboard.layout.tsx
+++ tsuka-group-proj/src/components/layouts/dashboard.layout.tsx
@@ -33,14 +33,17 @@ export const DashboardLayout: React.FC<PropsWithChildren> = ({ children }) => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="bg-tsuka-700 min-h-screen">
-        <nav>
-          <div className="w-full px-2 sm:px-6 lg:px-8 border-b border-tsuka-500">
-            <div className="relative flex h-16 items-center justify-between">
-              <div className="flex flex-1 items-center sm:justify-start">
-                <div className="flex flex-shrink-0 items-center px-4 md:px-2">
-                  <p className="text-xl font-extrabold text-tsuka-100 ">Logo</p>
-                  {/* <img
+      <main>
+        <div>
+          <nav>
+            <div className="w-full px-2 sm:px-6 lg:px-8 border-b border-tsuka-500">
+              <div className="relative flex h-16 items-center justify-between">
+                <div className="flex flex-1 items-center sm:justify-start">
+                  <div className="flex flex-shrink-0 items-center px-4 md:px-2">
+                    <p className="text-xl font-extrabold text-tsuka-100 ">
+                      Logo
+                    </p>
+                    {/* <img
                     className="block h-8 w-auto lg:hidden"
                     src=""
                     alt="Logo"
@@ -50,50 +53,54 @@ export const DashboardLayout: React.FC<PropsWithChildren> = ({ children }) => {
                     src=""
                     alt="Logo"
                   /> */}
-                </div>
-                <div className="hidden sm:ml-6 sm:block">
-                  <div className="flex px-10 space-x-4">
-                    {navLinks?.map(({ path, title }) => (
-                      <HeaderLinkButton
-                        key={path}
-                        path={path}
-                        title={title}
-                        active={path === router.pathname}
-                      />
-                    ))}
+                  </div>
+                  <div className="hidden sm:ml-6 sm:block">
+                    <div className="flex px-10 space-x-4">
+                      {navLinks?.map(({ path, title }) => (
+                        <HeaderLinkButton
+                          key={path}
+                          path={path}
+                          title={title}
+                          active={path === router.pathname}
+                        />
+                      ))}
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div className="absolute inset-y-0 right-0 flex flex-row-reverse md:flex-row items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                <HeaderMenuButton
-                  callback={() => setShowNav((prev) => !prev)}
-                />
-                <HeaderNotificationButton
-                  callback={() => console.log("notification click")}
-                />
-                <HeaderWalletButton
-                  callback={() => console.log("wallet click")}
-                  wallet={{ label: "Darkdakgo.eth" }}
-                />
+                <div className="absolute inset-y-0 right-0 flex flex-row-reverse md:flex-row items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                  <HeaderMenuButton
+                    callback={() => setShowNav((prev) => !prev)}
+                  />
+                  <HeaderNotificationButton
+                    callback={() => console.log("notification click")}
+                  />
+                  <HeaderWalletButton
+                    callback={() => console.log("wallet click")}
+                    wallet={{ label: "Darkdakgo.eth" }}
+                  />
+                </div>
               </div>
             </div>
-          </div>
-          {/* Mobile menu, show/hide based on menu state. */}
-          <div className={`${!showNav && "hidden"} sm:hidden`} id="mobile-menu">
-            <div className="space-y-1 px-2 pb-3 pt-2">
-              {navLinks?.map(({ path, title }) => (
-                <HeaderLinkButton
-                  key={path}
-                  path={path}
-                  title={title}
-                  active={path === router.pathname}
-                  callback={() => setShowNav(false)}
-                />
-              ))}
+            {/* Mobile menu, show/hide based on menu state. */}
+            <div
+              className={`${!showNav && "hidden"} sm:hidden`}
+              id="mobile-menu"
+            >
+              <div className="space-y-1 px-2 pb-3 pt-2">
+                {navLinks?.map(({ path, title }) => (
+                  <HeaderLinkButton
+                    key={path}
+                    path={path}
+                    title={title}
+                    active={path === router.pathname}
+                    callback={() => setShowNav(false)}
+                  />
+                ))}
+              </div>
             </div>
-          </div>
-        </nav>
-        <div className="p-4 md:p-8">{children}</div>
+          </nav>
+          <div className="p-4 md:p-8 relative">{children}</div>
+        </div>
       </main>
     </>
   );
