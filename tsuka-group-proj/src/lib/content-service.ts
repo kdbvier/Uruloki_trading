aef3c4d804d13c2b47583d30581ddcdbd25bb322
highmountain12321 <marathonracer12321@gmail.com>
2023-04-18 08:31:33 -0300

    feat: content-service

---
+++ tsuka-group-proj/src/lib/content-service.ts
@@ -0,0 +1,50 @@
+import { IBenifitsItemFields, CONTENT_TYPE, IBenifitsFields } from "@/@types/generated/contentful.types";
+import { createClient } from "contentful";
+// import { config } from "dotenv";
+
+// /*
+//  * We tell TypeScript that those environment variables are always defined.
+//  * If you want to learn more about this madness, read:
+//  * https://www.typescriptlang.org/docs/handbook/declaration-merging.html#module-augmentation
+//  */
+// declare global {
+//   namespace NodeJS {
+//     interface ProcessEnv {
+//       CONTENTFUL_SPACE_ID: string;
+//       CONTENTFUL_ACCESS_TOKEN: string;
+//     }
+//   }
+// }
+
+// config();
+
+export default class ContentService {
+  static get instance() {
+    return new ContentService();
+  }
+
+  client = createClient({
+    space: process.env.CONTENTFUL_SPACE_ID||"imgksyjxr9j5",
+    accessToken: process.env.CONTENTFUL_ACCESS_TOKEN||"z6-HPwg_2dKJ8Is5vGFVGLdxScENR7l325JFXVRlBbQ",
+  });
+
+  async getEntriesByType<T>(type: string) {
+    // async getEntriesByType<T extends EntrySkeletonType<FieldsType, string>>(type: string) {
+    return (
+      await this.client.getEntries<T>({
+        content_type: type,
+      })
+    ).items;
+  }
+}
+
+export async function getBenifitItemsEntries() {
+  const a = (
+    // await ContentService.instance.getEntriesByType<IBenifitsItemFields>("benifitsItem")
+    await ContentService.instance.getEntriesByType<IBenifitsFields>("benifits")
+  ).map((entry) => {
+    return entry.fields;
+  });
+  console.log("log", a);
+}
+
--- tsuka-group-proj/src/pages/index.tsx
+++ tsuka-group-proj/src/pages/index.tsx
@@ -1,10 +1,15 @@
 import { formItensData } from "@/@fake-data/form.fake-data";
 import { tokensData } from "@/@fake-data/token.fake-data";
+import { IBenifitsItemFields } from "@/@types/generated/contentful.types";
 import { AllPositionsToken } from "@/components/tokens/all-positions.token";
 import { CompareTokenChainToken } from "@/components/tokens/compare-token-chain.token";
 import { LiveGraphToken } from "@/components/tokens/live-graph.token";
 import { OrderWidgetToken } from "@/components/tokens/order-widget.token";
 import { PoolInfoToken } from "@/components/tokens/pool-info.token";
+import ContentService, {getBenifitItemsEntries} from "@/lib/content-service";
+// import { getEntries } from "@/lib/contentful_1";
+
+import { useEffect } from "react";
 
 export default function Home() {
   const titles = formItensData;
@@ -13,7 +18,15 @@ export default function Home() {
     return { id: token.id, token: token.chain.code, icon: token.chain.icon };
   });
 
+  useEffect(() => {
+    // getEntries().then((items)=> console.error("items:::", items));
+    // const entries = (await (ContentService.instance.getEntriesByType<IBenifitsItemFields>("article"))).map((entry: { fields: any; })=>entry.fields);
+    // console.log(entries);
+    getBenifitItemsEntries();
+  }, [])
+
   const networks = ["ETH", "BSC", "POLYGON"];
+  
 
   return (
     <div className="flex flex-col">
