5b41c9394777b4c49465103716ae0cf05f1b3b35
highmountain12321 <marathonracer12321@gmail.com>
2023-04-18 15:07:29 -0300

    fix: using process.env

--- tsuka-group-proj/src/lib/content-service.ts
+++ tsuka-group-proj/src/lib/content-service.ts
@@ -1,31 +1,39 @@
-import { IBenifitsItemFields, CONTENT_TYPE, IBenifitsFields } from "@/@types/generated/contentful.types";
+import {
+  IBenifitsItemFields,
+  CONTENT_TYPE,
+  IBenifitsFields,
+} from "@/@types/generated/contentful.types";
 import { createClient } from "contentful";
-// import { config } from "dotenv";
+import { config } from "dotenv";
 
-// /*
-//  * We tell TypeScript that those environment variables are always defined.
-//  * If you want to learn more about this madness, read:
-//  * https://www.typescriptlang.org/docs/handbook/declaration-merging.html#module-augmentation
-//  */
-// declare global {
-//   namespace NodeJS {
-//     interface ProcessEnv {
-//       CONTENTFUL_SPACE_ID: string;
-//       CONTENTFUL_ACCESS_TOKEN: string;
-//     }
-//   }
-// }
+/*
+ * We tell TypeScript that those environment variables are always defined.
+ * If you want to learn more about this madness, read:
+ * https://www.typescriptlang.org/docs/handbook/declaration-merging.html#module-augmentation
+ */
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      CONTENTFUL_SPACE_ID: string;
+      CONTENTFUL_ACCESS_TOKEN: string;
+    }
+  }
+}
 
-// config();
+config();
 
 export default class ContentService {
   static get instance() {
     return new ContentService();
   }
 
+  // client = createClient({
+  //   space: process.env.CONTENTFUL_SPACE_ID||"imgksyjxr9j5",
+  //   accessToken: process.env.CONTENTFUL_ACCESS_TOKEN||"z6-HPwg_2dKJ8Is5vGFVGLdxScENR7l325JFXVRlBbQ",
+  // });
   client = createClient({
-    space: process.env.CONTENTFUL_SPACE_ID||"imgksyjxr9j5",
-    accessToken: process.env.CONTENTFUL_ACCESS_TOKEN||"z6-HPwg_2dKJ8Is5vGFVGLdxScENR7l325JFXVRlBbQ",
+    space: process.env.CONTENTFUL_SPACE_ID,
+    accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
   });
 
   async getEntriesByType<T>(type: string) {
@@ -37,4 +45,3 @@ export default class ContentService {
     ).items;
   }
 }
-
