93ea47c41682a8dca1bee92ef3bf37c12fbb5d1b
kkingsbe <kkingsbe@gmail.com>
2023-04-19 07:56:15 -0400

    Minor improvements

--- tsuka-group-proj/src/pages/index.tsx
+++ tsuka-group-proj/src/pages/index.tsx
@@ -12,14 +12,14 @@ import ContentService from "@/lib/content-service";
 type newProps = {
   landingPages: ILandingPage[];
 }
-export default function Home({landingPages}: newProps){
+export default function Home({landingPage}: newProps){
   const titles = formItensData;
   const data = tokensData;
   const [inputToken, outputToken] = data.map((token) => {
     return { id: token.id, token: token.chain.code, icon: token.chain.icon };
   });
 
-  console.log(landingPages);
+  console.log(landingPage);
   const networks = ["ETH", "BSC", "POLYGON"];
   
 
@@ -49,12 +49,10 @@ export default function Home({landingPages}: newProps){
 }
 
 export const getServerSideProps =async () => {
-  const landingPageContents = await ContentService.instance.getEntriesByType<ILandingPage>("landingPage");
-  const landingPages = landingPageContents.map(entry=>{
-    return entry.fields;
-  });
+  const landingPageContents = (await ContentService.instance.getEntriesByType<ILandingPage>("landingPage"))[0];
+  const landingPage = landingPageContents.fields;
 
   getLandingPage().then(response=> console.log("response GraphQL:: ", response));
-  console.log("Inside ServerSideProps", landingPages);
-  return {props: {landingPages}}
+  console.log("Inside ServerSideProps", landingPage);
+  return {props: {landingPage}}
 }
