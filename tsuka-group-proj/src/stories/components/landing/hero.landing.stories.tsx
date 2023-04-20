a133360c77088006d1050eb6a55364e1282a190b
highmountain12321 <marathonracer12321@gmail.com>
2023-04-20 20:23:59 -0300

    ui: hero landing component

---
+++ tsuka-group-proj/src/stories/components/landing/hero.landing.stories.tsx
@@ -0,0 +1,24 @@
+import {
+  HeroProps,
+  HeroLanding as HeroLandingComponent,
+} from "@/components/landing/hero.landing";
+
+import { Meta, StoryObj } from "@storybook/react";
+
+export default {
+  title: "Landing/HeroLanding",
+  component: HeroLandingComponent,
+  args: {
+    title: "All-in-one platform for decentralized asset trading and arbitrage",
+    beforeHeroText: "Automate Your Crypto Trades with Uruloki",
+    afterHeroText:
+      "Our platform allows you to create an order book for decentralized assets, and group multiple orders into a single strategy for arbitraging between assets.",
+    image: {
+      url: "https://images.ctfassets.net/imgksyjxr9j5/4Rg2kkoMiGufVe6zDzlSLA/c74d8f75172217d8445962ed24c2ef70/3_2_screen_mockup.png",
+      width: 2106,
+      height: 1536,
+    },
+  },
+} as Meta<HeroProps>;
+
+export const HeroLanding: StoryObj<HeroProps> = {};
