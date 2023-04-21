73f31a884c7245cb6dd6d95081e9f6fd11390e15
highmountain12321 <marathonracer12321@gmail.com>
2023-04-21 19:35:31 -0300

    ui: header section modified with skew, titles modified

--- tsuka-group-proj/src/components/landing/hero.section.tsx
+++ tsuka-group-proj/src/components/landing/hero.section.tsx
@@ -48,27 +48,81 @@ export const HeroLanding: React.FC<HeroProps> = ({
         beforeMainText={beforeHeroText}
         afterMainText={afterHeroText}
       />
-      <div className="text-tsuka-100 mb-4 md:mb-0">
+      <div className="text-tsuka-100 mb-4 md:mb-0 z-10">
         <div className="flex justify-center mt-10">
-          <div className="inline-block bg-[#ffffff44] rounded-full p-[1px] mr-4">
+          <div className="bg-[#ffffff44] rounded-full p-[1px] mr-4 flex justify-center items-center">
             <input
               type={"email"}
-              className="h-full text-white bg-tsuka-500 text-base font-normal rounded-full pl-[12px] py-[6px] pr-[18px]"
+              className="h-full w-[312px] text-white bg-black text-base font-normal rounded-full pl-[23px] py-[17.5px] pr-[14px]"
               placeholder="Enter Your Email"
             ></input>
           </div>
           <div className="inline-block bg-gradient-to-r from-[#003525] to-[#31C699] rounded-full p-[1px] justify-center items-center">
-            <button className="h-full text-white bg-tsuka-500 text-base font-normal text-center rounded-full pl-[12px] py-[6px] pr-[12px]">
+            <button className="h-full text-white bg-black text-base font-normal text-center rounded-full pl-[38.5px] py-[17.5px] pr-[38.5px]">
               Get Started
             </button>
           </div>
         </div>
-        <Image
-          src={image.url}
-          alt="hero_image"
-          width={image.width}
-          height={image.height}
-        />
+        <div className="relative w-full flex justify-center">
+          <BlurLanding
+            width={519}
+            height={519}
+            left={752}
+            top={0}
+            blurSize={150}
+            circles={[
+              {
+                radius: 519,
+                left: 0,
+                top: 0,
+                color: "#013E2B",
+              },
+            ]}
+          />
+          <div className="absolute top-0 left-[-6px] w-full">
+            <div
+              className="absolute left-[-249px] top-[287px] w-[570px] h-[65.46px]"
+              style={{
+                background:
+                  "linear-gradient(270deg, #31C699 -3.19%, #003525 95.62%)",
+                transform:"skewY(-7deg)"
+              }}
+            ></div>
+            <div
+              className="absolute left-[-80px] top-[208.11px] w-[466.67px] h-[65.46px]"
+              style={{
+                boxSizing: 'border-box',
+                borderTop: '1px solid',
+                borderTopColor: "linear-gradient(270deg, #31C699 -3.19%, #003525 95.62%)",
+                transform:"skewY(-7deg)"
+              }}
+            ></div>
+            <div
+              className="absolute left-[1017.78px] top-[50px] w-[466.67px] h-[65.46px]"
+              style={{
+                background:
+                'linear-gradient(88.02deg, #31C699 0.85%, #003525 95.39%)',
+                transform:"skewY(-7deg)"
+              }}
+            ></div>
+            <div
+              className="absolute left-[840px] top-[116.44px] w-[680px] h-[65.46px]"
+              style={{
+                boxSizing: 'border-box',
+                borderBottom: '1px solid',
+                borderBottomColor: 'linear-gradient(88.02deg, #31C699 0.85%, #003525 95.39%)',
+                transform:"skewY(-7deg)"
+              }}
+            ></div>
+          </div>
+          <Image
+            src={image.url}
+            alt="hero_image"
+            width={1024}
+            height={1}
+            style={{ position: "relative" }}
+          />
+        </div>
       </div>
     </div>
   );
--- tsuka-group-proj/src/components/landing/sectiontitle.landing.tsx
+++ tsuka-group-proj/src/components/landing/sectiontitle.landing.tsx
@@ -16,16 +16,16 @@ export const SectionTitle: React.FC<SectionTitleProps> = ({
 
       <div className="px-28 justify-center text-center relative">
         <div className="inline-block bg-gradient-to-r from-[#003525] to-[#31C699] rounded-full p-[1px]">
-          <h1 className=" text-white bg-tsuka-500 text-xs font-normal text-center rounded-full pl-[12px] py-[6px] pr-[18px]">
+          <h1 className=" text-white bg-black text-xs leading-[150%] font-normal text-center rounded-full pl-[12px] py-[6px] pr-[18px]">
             {beforeMainText}
           </h1>
         </div>
         <div className="px-14 mt-5 bg-transparent z-40">
-          <span className="block m-auto text-6xl font-semibold text-center text-white z-50">
+          <span className="block m-auto text-6xl leading-[120%] font-semibold text-center text-white z-50">
             {MainText}
           </span>
         </div>
-        <div className=" text-tsuka-100 text-base px-72 mt-5 mb-8 md:mb-0">
+        <div className=" text-tsuka-100 text-base leading-[200%] px-72 mt-5 mb-8 md:mb-0">
           {afterMainText}
         </div>
         
