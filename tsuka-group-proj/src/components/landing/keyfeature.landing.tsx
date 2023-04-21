e9f9bfd61789d71e2099aaa246c33a0574a60f40
highmountain12321 <marathonracer12321@gmail.com>
2023-04-21 04:53:32 -0300

    ui: created keyfeature card

--- tsuka-group-proj/src/components/landing/keyfeature.landing.tsx
+++ tsuka-group-proj/src/components/landing/keyfeature.landing.tsx
@@ -16,14 +16,12 @@ export const KeyFeatureCard: React.FC<KeyFeatureCardProps> = ({
   icon,
 }) => {
   return (
-    <div className="w-full h-80 bg-red-900 bg-opacity-3 border backdrop-blur-15 rounded-30 pl-[38px] pr-[24px] pt-[51px] pb-[42px]">
-      <div className="w-[44px] h-[40px] bg-slate-600 flex justify-center items-center rounded-3xl">
-        <Image src={icon.url} alt="keyfeature__image" width={24} height={24}/>
+    <div className="w-full h-80 bg-white bg-opacity-[0.03] border border-white border-opacity-[0.12] backdrop-blur-[15px] rounded-3xl pl-[38px] pr-[24px] pt-[51px] pb-[42px]">
+      <div className="w-[56px] h-[57px] bg-white bg-opacity-5 backdrop-blur-[10px] flex justify-center items-center rounded-xl">
+        <Image src={icon.url} alt="keyfeature__image" width={20} height={18}/>
       </div>
-      <h1 className="text-white text-xl font-semibold">{title}</h1>
-      <h1 className="text-white text-xl font-semibold font-gilroy">{title}</h1>
-      <h1 className="text-white text-xl font-semibold font-mono">{title}</h1>
-      <h4 className="text-white">{description}</h4>
+      <h1 className="text-white text-xl leading-8 font-semibold mt-7 pr-12">{title}</h1>
+      <h4 className="text-[#ADADAD] mt-2 pr-12 text-[15px] leading-7">{description}</h4>
     </div>
   );
 };
