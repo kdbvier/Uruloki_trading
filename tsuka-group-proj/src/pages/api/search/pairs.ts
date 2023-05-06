33fcd9fbd8e5815f7460caae1e93e248da7fb1a5
highmountain12321 <marathonracer12321@gmail.com>
2023-05-06 12:21:06 -0300

    feat: api route for search for token and pairs

--- tsuka-group-proj/src/pages/api/search/pairs.ts
+++ tsuka-group-proj/src/pages/api/search/pairs.ts
@@ -1,8 +1,8 @@
-/** route example: api/search/token?name=usdt */
+/** route example: api/search/pairs?token0=0xdac17f958d2ee523a2206206994597c13d831ec7 */
 
 import type { ApiResponse } from "@/types";
 import type { NextApiRequest, NextApiResponse } from "next";
-import { searchTokensByName } from "@/lib/search_4";
+import { getPairsByTokenAddress } from "@/lib/search_4";
 
 export default async function orderHandler(
   req: NextApiRequest,
@@ -13,11 +13,18 @@ export default async function orderHandler(
     case "GET":
       try {
         // const orders = await prisma.orders.findMany({});
-        let name = query.name as string;
-        const tokens = await searchTokensByName(name);
-        res
-          .status(200)
-          .json({ payload: tokens, message: `Successfully found orders` });
+        let token0 = query.token0 as string;
+        const tokens = await getPairsByTokenAddress(token0);
+        if(tokens.length===0){
+          res.status(404).json({
+            payload:undefined,
+            message: `No pairs found with token address ${token0}`
+          })
+        } else {
+          res
+            .status(200)
+            .json({ payload: tokens, message: `Successfully found orders` });
+        }
       } catch (err) {
         res.status(400).json({
           payload: undefined,
