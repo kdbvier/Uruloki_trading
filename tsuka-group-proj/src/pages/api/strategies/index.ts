3727f3aeb13ddfbd926b27d0f929b5947d3e6994
smartcoder0305 <102741052+smartcoder0305@users.noreply.github.com>
2023-05-08 22:47:25 +0200

    # fix strategies get all api

--- tsuka-group-proj/src/pages/api/strategies/index.ts
+++ tsuka-group-proj/src/pages/api/strategies/index.ts
@@ -72,33 +72,65 @@ export default async function strategyHandler(
       break;
     case "GET":
       try {
-        const strategies = await prisma.strategies.findMany({});
-        const payload = await Promise.all(
-          strategies.map(async (strategy) => {
-            const { orders, orderTokens } = await getStrategy(
-              strategy.strategy_id.toString()
-            );
-            return {
-              id: strategy.strategy_id.toString(),
-              title: strategy.name as string,
-              status: strategy.status as StrategyStatusEnum,
-              createdAt: Math.round(
-                (strategy.createdAt?.getTime() ?? 0) / 1000
-              ).toString() as string,
-              orderTokens: orderTokens.map((orderToken) => ({
+        const strategies = await prisma.strategies.findMany({
+          include: {
+            order_strategy: {
+              select: {
+                order: true,
+              },
+            },
+          },
+        });
+        let orders: any = {};
+        for (const strategy of strategies) {
+          const { order_strategy, strategy_id, ...rest } = strategy;
+          for (const { order } of order_strategy) {
+            if (!orders[strategy_id]) orders[strategy_id] = {};
+            if (orders[strategy_id][order.pair_address]) {
+              orders[strategy_id][order.pair_address].push(order);
+            } else {
+              orders[strategy_id][order.pair_address] = [order];
+            }
+          }
+        }
+        const payload = strategies.map((strategy) => ({
+          id: strategy.strategy_id.toString(),
+          title: strategy.name as string,
+          status: strategy.status as StrategyStatusEnum,
+          createdAt: Math.round(
+            (strategy.createdAt?.getTime() ?? 0) / 1000
+          ).toString() as string,
+          orderTokens: orders[strategy.strategy_id]
+            ? Object.keys(orders[strategy.strategy_id]).map((pair_address) => ({
                 network: "Ethereum",
-                name1: orderToken.baseTokenLongName as string,
-                code1: orderToken.baseTokenShortName as string,
-                name2: orderToken.pairTokenLongName as string,
-                code2: orderToken.baseTokenShortName as string,
-                status: orderToken.status as OrderStatusEnum,
-                orders: orders[orderToken.order_id] as Array<
-                  SingleOrder | RangeOrder
-                >,
-              })),
-            };
-          })
-        );
+                name1: orders[strategy.strategy_id][pair_address][0]
+                  .baseTokenLongName as string,
+                code1: orders[strategy.strategy_id][pair_address][0]
+                  .baseTokenShortName as string,
+                name2: orders[strategy.strategy_id][pair_address][0]
+                  .pairTokenLongName as string,
+                code2: orders[strategy.strategy_id][pair_address][0]
+                  .baseTokenShortName as string,
+                status: orders[strategy.strategy_id][pair_address][0]
+                  .status as OrderStatusEnum,
+                orders: orders[strategy.strategy_id][pair_address].map(
+                  (order: any) => ({
+                    id: order.order_id,
+                    budget: order.budget,
+                    price_type: order.price_type,
+                    order_type: order.order_type,
+                    status: order.status,
+                    baseTokenShortName: order.baseTokenShortName,
+                    baseTokenLongName: order.baseTokenLongName,
+                    pairTokenShortName: order.pairTokenShortName,
+                    pairTokenLongName: order.pairTokenLongName,
+                    price: order.single_price,
+                    prices: [order.from_price, order.to_price],
+                  })
+                ),
+              }))
+            : [],
+        }));
         res.status(200).json({
           payload,
           message: `Successfully found setups`,
