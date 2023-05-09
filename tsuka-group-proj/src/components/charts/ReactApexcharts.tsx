df4c752c4dc19dd29b029f3c7e6e341d7f6a2010
lovely0806 <star.software.guy@gmail.com>
2023-05-09 22:31:37 +0200

    customize the chart

--- tsuka-group-proj/src/components/charts/ReactApexcharts.tsx
+++ tsuka-group-proj/src/components/charts/ReactApexcharts.tsx
@@ -11,94 +11,102 @@ const donutColors = {
   series2: "#EB5757",
 };
 
+const chartData = {
+  total: 5251501,
+  active: 42501,
+  out: 10211,
+};
+
+const changeString = (val: number) => {
+  const res = val.toLocaleString();
+  return res;
+};
+
 const Chart = () => {
   const options: ApexOptions = {
-    stroke: { width: 0 },
-    labels: ["4,250,1", "1,027.1"],
-    colors: [donutColors.series1, donutColors.series2],
-    dataLabels: {
-      enabled: true,
-      formatter: (val: string) => val,
+    chart: {
+      type: "donut",
+      height: 150,
+      width: "100%",
     },
-    legend: {
-      position: "bottom",
-      markers: { offsetX: -5 },
-      itemMargin: {
-        vertical: 5,
-        horizontal: 5,
-      },
+    stroke: {
+      width: 5,
+      colors: ["#1F2333"],
     },
     plotOptions: {
       pie: {
+        startAngle: 50,
         donut: {
+          size: "85%",
           labels: {
             show: true,
             name: {
-              fontSize: "1.2rem",
+              show: true,
+              offsetY: 38,
+              formatter: () => "Total Orders",
             },
             value: {
-              fontSize: "1.2rem",
-              formatter: (val: string) => val,
+              show: true,
+              fontSize: "32px",
+              fontFamily: "DM Sans",
+              fontWeight: 400,
+              color: "rgba(255, 255, 255, 0.7)",
+              offsetY: -10,
             },
             total: {
               show: true,
-              fontSize: "1.4rem",
-              label: "5,251,501",
-              formatter: () => "Total Orders",
+              showAlways: true,
+              color: "rgba(255, 255, 255, 0.5)",
+              fontFamily: "Open Sans",
+              fontSize: "16px",
+              fontWeight: 400,
+              formatter: (val: string) => "5,251,501",
             },
           },
         },
       },
     },
-    responsive: [
-      {
-        breakpoint: 992,
-        options: {
-          chart: {
-            height: 380,
-          },
-          legend: {
-            position: "bottom",
-          },
-        },
+    dataLabels: {
+      enabled: false,
+    },
+    labels: ["Active", "Out of Funds"],
+    legend: {
+      show: false,
+      position: "right",
+      markers: { offsetX: 0 },
+      itemMargin: {
+        vertical: 0,
+        horizontal: 0,
       },
-      {
-        breakpoint: 576,
-        options: {
-          chart: {
-            height: 320,
-          },
-          plotOptions: {
-            pie: {
-              donut: {
-                labels: {
-                  show: true,
-                  name: {
-                    fontSize: "1rem",
-                  },
-                  value: {
-                    fontSize: "1rem",
-                  },
-                  total: {
-                    fontSize: "1rem",
-                  },
-                },
-              },
-            },
-          },
-        },
-      },
-    ],
+    },
+
+    colors: [donutColors.series1, donutColors.series2],
   };
 
   return (
-    <div className="bg-[#1F2333] rounded-md py-[21] px-[18.75 ] mb-3">
-      <ReactApexcharts
-        type="donut"
-        height={300}
-        options={options}
-        series={[80, 20]}
-      />
+    <div className="bg-[#1F2333] rounded-md py-[21] px-[18.75 ] mb-3 relative">
+      <div className="mr-10">
+        <ReactApexcharts
+          type="donut"
+          height={220}
+          options={options}
+          series={[80, 20]}
+        />
+      </div>
+      <div className="flex flex-col absolute top-[55px] right-3">
+        <div className="font-['DM Sans'] font-medium text-[10px] leading-[13px] text-[#6FCF97]">
+          Active
+        </div>
+        <div className="font-['DM Sans'] font-medium text-[16px] leading-[21px] text-[#BBC3D7]">
+          {changeString(chartData.active)}
+        </div>
+        <div className="font-['DM Sans'] font-medium text-[10px] leading-[13px] text-[#EB5757] mt-5">
+          OUt Of Funds
+        </div>
+        <div className="font-['DM Sans'] font-medium text-[16px] leading-[21px] text-[#BBC3D7]">
+          {changeString(chartData.out)}
+        </div>
+      </div>
     </div>
   );
 };
