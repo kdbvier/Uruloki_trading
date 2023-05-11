328bc910650393acbe08bbeea1facf9b3571199d
ITExpert0228 <dhv.techcenter@gmail.com>
2023-05-12 03:16:51 +0700

    Update index.tsx

--- tsuka-group-proj/src/pages/profile/index.tsx
+++ tsuka-group-proj/src/pages/profile/index.tsx
@@ -14,13 +14,13 @@ import { getConnectedAddress } from "@/helpers/web3Modal";
 type PageProps = {
   tokenBalances: Array<CardType>;
   chartData: ChartType;
-  walletBalances: Array<CardType>;
 };
-export default function Profile({ tokenBalances, chartData, walletBalances }: PageProps) {
+export default function Profile({ tokenBalances, chartData }: PageProps) {
   const [searchValue, setSearchValue] = useState<string>("");
   const [showModal, setShowModal] = useState<boolean>(false);
   const [isDeposit, setIsDeposit] = useState<boolean>(false);
   const [walletAddress, setWalletAddress] = useState<string>("");
+  const [walletBalances, setWalletBalances] = useState<Array<CardType>>([]);
 
   const handleOpenWidrawModal = () => {
     setShowModal(true);
@@ -33,10 +33,24 @@ export default function Profile({ tokenBalances, chartData, walletBalances }: Pa
   };
   useEffect(() => {
     console.log("useEffect");
+    const tokensInWallet = async(address: string) => {
+      try {
+        const res = await fetch(`/api/tokens/tokens-in-wallet?walletAddress=${address}`);
+        const data = await res.json();
+        console.log(data);
+        return data
+      } catch (err) {
+        console.log(err);
+      }
+      return null;
+    }
     const getAddress = async () => {
       try {
         const address: string | null = await getConnectedAddress();
         setWalletAddress(address);
+        // const data = await tokensInWallet("0x28Dc1b43ebCd1A0A0B5AB1E25Fac0b82551207ef");
+        const data = await tokensInWallet(address);
+        setWalletBalances(data.payload.walletBalances);
       } catch (error) {
         console.log(error);
       }
@@ -226,8 +240,8 @@ export async function getServerSideProps() {
   // Fetch data from external API
   const getCardsData = await getCards();
   const chartData = await getChartData();
-  const tokensInWallet = await getTokensInWallet("0x28Dc1b43ebCd1A0A0B5AB1E25Fac0b82551207ef")
+  //const tokensInWallet = await getTokensInWallet("0x28Dc1b43ebCd1A0A0B5AB1E25Fac0b82551207ef")
 
   // Pass data to the page via props
-  return { props: { tokenBalances: getCardsData, chartData: chartData, walletBalances: tokensInWallet } };
+  return { props: { tokenBalances: getCardsData, chartData: chartData } };
 }
