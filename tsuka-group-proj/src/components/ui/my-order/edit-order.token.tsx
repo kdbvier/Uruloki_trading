9de0253193ee64eb09defbfc31a6f9922f3168be
smartcoder0305 <102741052+smartcoder0305@users.noreply.github.com>
2023-05-12 04:12:15 +0200

    # fix number format

--- tsuka-group-proj/src/components/ui/my-order/edit-order.token.tsx
+++ tsuka-group-proj/src/components/ui/my-order/edit-order.token.tsx
@@ -22,6 +22,7 @@ import { FiX, FiPlusCircle } from "react-icons/fi";
 import ToggleButton from "../buttons/toggle.button";
 import { FaClock, FaSync } from "react-icons/fa";
 import { getAllTokenCache } from "@/store/apps/token-cache";
+import { formatNumberToHtmlTag } from "@/helpers/coin.helper";
 
 export interface EditOrderTokenProp {
   isEdit?: boolean;
@@ -154,8 +155,25 @@ export const EditOrderToken: React.FC<EditOrderTokenProp> = ({
     },
   ];
 
+  const convertLawPrice = (price: number) => {
+    let priceEle;
+    if (price >= 0.01) {
+      priceEle = `$${commafy(price)}`;
+    } else {
+      priceEle = (
+        <>
+          ${formatNumberToHtmlTag(price).integerPart}
+          .0
+          <sub>{formatNumberToHtmlTag(price).leadingZerosCount}</sub>
+          {formatNumberToHtmlTag(price).remainingDecimal}
+        </>
+      );
+    }
+    return priceEle;
+  };
+
   const handleNumberInputChange = (name: string, event: any) => {
-    const value = event.target.value.replace(/,/g, "");
+    let value = event.target.value.replace(/,/g, "");
     const pattern = /^\d*\.?\d*$/;
     if (!pattern.test(value)) return;
     let newValue = "";
@@ -167,7 +185,6 @@ export const EditOrderToken: React.FC<EditOrderTokenProp> = ({
     } else {
       newValue = value.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
     }
-    newValue = newValue || "0";
     switch (name) {
       case "amount":
         setAmount(newValue);
@@ -283,7 +300,8 @@ export const EditOrderToken: React.FC<EditOrderTokenProp> = ({
           <p className="text-sm">
             <span className="text-tsuka-200">Current Price : </span>
             <span className="text-tsuka-50">
-              ${handleNumberFormat(Number(token_price.base_price?.toFixed(2)))}
+              {!!token_price.base_price &&
+                convertLawPrice(token_price.base_price)}
             </span>
           </p>
           {/* <div className="w-full mt-4 flex gap-2 text-sm">
@@ -516,15 +534,18 @@ export const EditOrderToken: React.FC<EditOrderTokenProp> = ({
                 <span className="text-custom-primary text-xs"> MAX</span>
               </p>
               <span className="text-tsuka-50 text-sm">
-                $
-                {handleNumberFormat(
-                  parseFloat(
-                    (
-                      parseFloat(amount.split(",").join("")) *
-                      (isBuy ? token_price.quote_price : token_price.base_price)
-                    ).toFixed(2)
-                  )
-                )}
+                {(() => {
+                  let totalAmount =
+                    parseFloat(amount.split(",").join("")) *
+                    (isBuy ? token_price.quote_price : token_price.base_price);
+                  return totalAmount
+                    ? totalAmount >= 0.001
+                      ? `$${handleNumberFormat(
+                          parseFloat(totalAmount.toFixed(3))
+                        )}`
+                      : convertLawPrice(totalAmount)
+                    : "$0";
+                })()}
               </span>
             </div>
           </div>
