ac7c839fa4904d15c573c7da79590322bf70b2ec
João Gustavo Camilo Júnior <joaogcamilojr@gmail.com>
2023-05-13 02:03:18 -0300

    fix: merge conflicts

--- tsuka-group-proj/src/pages/pair/[pair_id].tsx
+++ tsuka-group-proj/src/pages/pair/[pair_id].tsx
@@ -1,5 +1,4 @@
 import { tokensData } from "@/@fake-data/token.fake-data";
-import { SidebarStrategies } from "@/components/strategies/sidebar.strategies";
 import { LiveGraphToken } from "@/components/tokens/live-graph.token";
 import { OrderBookToken } from "@/components/tokens/order-book.token";
 import { OrderWidgetToken } from "@/components/tokens/order-widget.token";
@@ -23,7 +22,6 @@ import { Token } from "@/types/token.type";
 import { useRouter } from "next/router";
 import { useEffect, useMemo, useState } from "react";
 import { FiPlusCircle } from "react-icons/fi";
-import { HiOutlineArrowLongLeft } from "react-icons/hi2";
 
 interface InputToken {
   id: string;
@@ -87,99 +85,80 @@ export default function Pair({ tranData }: any, { id }: { id: string }) {
 
   return (
     <div className="flex flex-col px-4 md:px-10 py-6">
-      {token && (
-        <>
-          <FullHeaderToken token={token} pair_address={String(pair_address)} />
-          <div className="hidden lg:grid grid-cols-11 gap-4">
-            {/* <div className="col-span-12 md:col-span-3">
+      <FullHeaderToken pair_address={String(pair_address)} />
+      <div className="hidden lg:grid grid-cols-11 gap-4">
+        {/* <div className="col-span-12 md:col-span-3">
               <CompareTokenChainToken token={token} networks={networks} />
             </div> */}
-            <div className="col-span-12 md:col-span-8">
-              <LiveGraphToken />
-              {token && (
-                <div className="hidden md:grid grid-cols-8 gap-4">
-                  <div className="col-span-12 md:col-span-3">
-                    <PoolInfoToken token={token} />
-                  </div>
-                  <div className="col-span-12 md:col-span-5">
-                    <OrderBookToken token={token} />
-                  </div>
-                </div>
-              )}
-            </div>
-            <div className="col-span-12 md:col-span-3">
-              {currentToken && compareToken && (
-                <>
-                  <DefaultButton
-                    label="Create an Order"
-                    callback={() => setShowEditOrderModal(true)}
-                    filled={true}
-                    Icon={FiPlusCircle}
-                  />
-                  <OrderWidgetToken
-                    name1={currentToken?.chain.name as string}
-                    code1={currentToken?.chain.code as string}
-                    name2={compareToken?.chain.name as string}
-                    code2={compareToken?.chain.code as string}
-                    status={statusOrder}
-                    orders={orders}
-                  />
-                </>
-              )}
-            </div>
-          </div>
-          <div className="block lg:hidden">
-            <LiveGraphToken />
-            {token && (
-              <>
-                {currentToken && compareToken && orders && (
-                  <OrderWidgetToken
-                    name1={currentToken?.chain.name as string}
-                    code1={currentToken?.chain.code as string}
-                    name2={compareToken?.chain.name as string}
-                    code2={compareToken?.chain.code as string}
-                    status={statusOrder}
-                    orders={orders}
-                  />
-                )}
-                <OrderBookToken token={token} />
+        <div className="col-span-12 md:col-span-8">
+          <LiveGraphToken />
+          {token && (
+            <div className="hidden md:grid grid-cols-8 gap-4">
+              <div className="col-span-12 md:col-span-3">
                 <PoolInfoToken token={token} />
-              </>
-            )}
-          </div>
-          {showEditOrderModal && (
-            <EditOrderToken
-              isEdit={false}
-              name1={currentToken?.chain.name as string}
-              code1={currentToken?.chain.code as string}
-              name2={compareToken?.chain.name as string}
-              code2={compareToken?.chain.code as string}
-              pair_address={id}
-              setShowEditOrderModal={setShowEditOrderModal}
-              selectedOrderId={0} //TODO: Fix this
-              closeHandler={() => {
-                setShowEditOrderModal(false);
-              }} //--//TODO: Fix this
-            />
+              </div>
+              <div className="col-span-12 md:col-span-5">
+                <OrderBookToken token={token} />
+              </div>
+            </div>
           )}
-          <div className="fixed z-10 bottom-4 right-4 bg-tsuka-300 text-tsuka-50 rounded-full text-sm font-normal whitespace-nowrap">
-            <button
-              type="button"
-              onClick={() => setShowSidebar(true)}
-              className="w-full text-center focus:outline-none rounded-full text-sm p-4 inline-flex justify-center items-center mr-2"
-            >
-              <label className="mr-2">
-                <HiOutlineArrowLongLeft size={24} />
-              </label>
-              Order & Strategies
-            </button>
-          </div>
-          <SidebarStrategies
-            open={showSidebar}
-            handleOpen={() => setShowSidebar(false)}
-            strategies={strategies!}
-          />
-        </>
+        </div>
+        <div className="col-span-12 md:col-span-3">
+          {currentToken && compareToken && (
+            <>
+              <DefaultButton
+                label="Create an Order"
+                callback={() => setShowEditOrderModal(true)}
+                filled={true}
+                Icon={FiPlusCircle}
+              />
+              <OrderWidgetToken
+                name1={currentToken?.chain.name as string}
+                code1={currentToken?.chain.code as string}
+                name2={compareToken?.chain.name as string}
+                code2={compareToken?.chain.code as string}
+                status={statusOrder}
+                orders={orders}
+              />
+            </>
+          )}
+        </div>
+      </div>
+      <div className="block lg:hidden">
+        <LiveGraphToken />
+        {token && (
+          <>
+            {currentToken && compareToken && orders && (
+              <OrderWidgetToken
+                name1={currentToken?.chain.name as string}
+                code1={currentToken?.chain.code as string}
+                name2={compareToken?.chain.name as string}
+                code2={compareToken?.chain.code as string}
+                status={statusOrder}
+                orders={orders}
+              />
+            )}
+            <OrderBookToken token={token} />
+            <PoolInfoToken token={token} />
+          </>
+        )}
+        <OrderBookToken token={token} />
+        <PoolInfoToken token={token} />
+      </div>
+      {showEditOrderModal && (
+        <EditOrderToken
+          isEdit={false}
+          name1={currentToken?.chain.name as string}
+          code1={currentToken?.chain.code as string}
+          name2={compareToken?.chain.name as string}
+          code2={compareToken?.chain.code as string}
+          pair_address={id}
+          setShowEditOrderModal={setShowEditOrderModal}
+          selectedOrderId={0} //TODO: Fix this
+          closeHandler={() => {
+            setShowEditOrderModal(false);
+          }} //--//TODO: Fix this
+        />
       )}
     </div>
   );
