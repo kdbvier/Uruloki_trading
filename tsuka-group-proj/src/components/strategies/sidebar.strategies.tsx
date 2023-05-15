ac486c48a6c8e18dc4098f8f353989ae8ceea92e
João Gustavo Camilo Júnior <joaogcamilojr@gmail.com>
2023-05-15 14:31:04 -0300

    fix: display strategies list

--- tsuka-group-proj/src/components/strategies/sidebar.strategies.tsx
+++ tsuka-group-proj/src/components/strategies/sidebar.strategies.tsx
@@ -3,7 +3,6 @@ import Link from "next/link";
 import { useState } from "react";
 import { MdArrowForward } from "react-icons/md";
 import { DefaultButton } from "../ui/buttons/default.button";
-import { StatusSpan } from "../ui/spans/status.span";
 import { TokenIconSpan } from "../ui/spans/token-icon.span";
 import { TokenPairStrategies } from "../ui/strategies/token-pair.strategies";
 
@@ -49,86 +48,78 @@ export const SidebarStrategies: React.FC<SidebarStrategiesProps> = ({
         </div>
         <div className="py-2">Showing list of setups</div>
         <div className="flex flex-col mt-4 gap-4">
-          {strategies.map(({ id, orderTokens }) =>
-            orderTokens?.map((item, index) => (
-              <div
-                key={index}
-                className="border border-tsuka-200 p-2 rounded-md flex align-top"
-              >
-                <div className="flex flex-1 flex-col">
-                  <Link href={`/strategies/${id}`}>
-                    <div className="flex">
-                      <div className="flex -space-x-3 mr-2">
-                        <TokenIconSpan code={item.code1} name={item.name1} />
-                        <TokenIconSpan code={item.code2} name={item.name2} />
-                        {item.extraTokens && (
-                          <span className="flex items-center justify-center w-7 h-7 text-xs font-medium text-tsuka-50 bg-tsuka-400 rounded-full">
-                            +{item.extraTokens.length}
-                          </span>
-                        )}
-                      </div>
-                      <div className="flex flex-col">
-                        <span className="text-tsuka-50 text-sm">
-                          {item.extraTokens
-                            ? "Avoid Strategy"
-                            : `${item.code1}/${item.code2}`}
+          {strategies.map(({ id, status, orderTokens }, index) => (
+            <div
+              key={id}
+              className="border border-tsuka-200 p-2 rounded-md flex align-top"
+            >
+              <div className="flex flex-1 flex-col">
+                <Link href={`/strategies/${id}`}>
+                  <div className="flex">
+                    <div className="flex -space-x-3 mr-2">
+                      <TokenIconSpan
+                        code={orderTokens[0]?.code1}
+                        name={orderTokens[0]?.name1}
+                      />
+                      <TokenIconSpan
+                        code={orderTokens[0]?.code2}
+                        name={orderTokens[0]?.name2}
+                      />
+                      {orderTokens?.length >= 2 && (
+                        <span className="flex items-center justify-center w-7 h-7 text-xs font-medium text-tsuka-50 bg-tsuka-400 rounded-full">
+                          +{orderTokens?.length}
                         </span>
-                        <span className="text-tsuka-200 text-xs">ID: {id}</span>
-                      </div>
-                    </div>
-                  </Link>
-                  {item.extraTokens && (
-                    <div className="ml-2 px-2 border-l-2 text-sm border-accent text-accent border-dashed">
-                      {showExtraId === index && (
-                        <>
-                          <div className="flex mt-2">
-                            <TokenPairStrategies
-                              id={id}
-                              name1={item.name1}
-                              code1={item.code1}
-                              name2={item.name2}
-                              code2={item.code2}
-                            />
-                          </div>
-                          {item.extraTokens.map((extra) => (
-                            <div key={extra.code} className="flex mt-2">
-                              <TokenPairStrategies
-                                id={id}
-                                name1={item.name1}
-                                code1={item.code1}
-                                name2={extra.name}
-                                code2={extra.code}
-                              />
-                            </div>
-                          ))}
-                        </>
                       )}
-                      <span
-                        onClick={() =>
-                          setShowExtraId((prev) =>
-                            prev === index ? null : index
-                          )
-                        }
-                        className="cursor-pointer"
-                      >
-                        {showExtraId === index
-                          ? ""
-                          : `${2 + item.extraTokens.length} orders in strategy`}
-                        <span className="text-xs font-light">
-                          {showExtraId === index
-                            ? "(Show less)"
-                            : "(Click to see)"}
-                        </span>
-                      </span>
                     </div>
-                  )}
-                </div>
-                <div>
-                  <StatusSpan status={item.status} />
-                </div>
+                    <div className="flex flex-col">
+                      <span className="text-tsuka-50 text-sm">
+                        {orderTokens?.length >= 2
+                          ? "Avoid Strategy"
+                          : `${orderTokens[0]?.code1}/${orderTokens[0]?.code2}`}
+                      </span>
+                      <span className="text-tsuka-200 text-xs">ID: {id}</span>
+                    </div>
+                  </div>
+                </Link>
+                {orderTokens?.length >= 2 && (
+                  <div className="ml-2 px-2 border-l-2 text-sm border-accent text-accent border-dashed">
+                    {showExtraId === index &&
+                      orderTokens?.map((extra) => (
+                        <div key={extra.code1} className="flex mt-2">
+                          <TokenPairStrategies
+                            id={id}
+                            name1={extra.name1}
+                            code1={extra.code1}
+                            name2={extra.name2}
+                            code2={extra.code2}
+                          />
+                        </div>
+                      ))}
+                    <span
+                      onClick={() =>
+                        setShowExtraId((prev) =>
+                          prev === index ? null : index
+                        )
+                      }
+                      className="cursor-pointer"
+                    >
+                      {showExtraId === index
+                        ? ""
+                        : `${orderTokens?.length} orders in strategy`}
+                      <span className="text-xs font-light">
+                        {showExtraId === index
+                          ? "(Show less)"
+                          : "(Click to see)"}
+                      </span>
+                    </span>
+                  </div>
+                )}
               </div>
-            ))
-          )}
+              {/* <div>
+                <StatusSpan status={status || orderTokens[0]?.status} />
+              </div> */}
+            </div>
+          ))}
         </div>
       </div>
     </div>
