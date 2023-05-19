49ec5ddd2cb6e324ea21ca11ed394c545af3132b
highmountain12321 <marathonracer12321@gmail.com>
2023-05-19 02:09:30 -0300

    fix: beautify strategies page

--- tsuka-group-proj/src/pages/strategies/index.tsx
+++ tsuka-group-proj/src/pages/strategies/index.tsx
@@ -1,38 +1,14 @@
 import { StrategyBookStrategies } from "@/components/strategies/strategy-book.strategies";
 import Strategies from "@/lib/api/strategies";
-import { getStrategies } from "@/store/apps/strategies";
-import { useAppDispatch, useAppSelector } from "@/store/hooks";
 import { Strategy } from "@/types";
 import Link from "next/link";
-import { useEffect } from "react";
 
 export async function getServerSideProps() {
-  const response = await Strategies.getStrategiesData();
-  console.log(response);
-  return { props: {data: response} }
+  const strategies = await Strategies.getStrategiesData();
+  return { props: {strategies:strategies} }
 }
 
-export default function StrategyDetails({data}:{data:Strategy[]}) {
-  console.log("data by serverside Props::: ", data);
-  // console.log("response by serverside props: ", response);
-  // const dispatch = useAppDispatch();
-  // const {
-  //   strategies: { value: strategies },
-  // } = useAppSelector((state) => state);
-
-  // useEffect(()=>{
-  //   Strategies.getStrategiesData().then(data=>{
-  //     console.log("dlfjdlfjlakjfldakjflakjfljgfgjfkhkghiurtjfgsjgf");
-  //     console.log(data);
-  //   })
-  // }, [])
-  // useEffect(() => {
-  //   dispatch(getStrategies());
-  // }, [dispatch]);
-  // const strategies = response;
-
-  const strategies = data as Strategy[]
-
+export default function StrategyDetails({strategies}:{strategies:Strategy[]}) {
   return (
     <div className="relative px-4 md:px-10 pt-3 md:pt-6 pb-8">
       <div className="w-full gap-4 text-tsuka-300 flex py-2 mb-2 md:items-center flex-row">
