b6aca57b149b6b6f6b98243db633152f979e7d24
Kyle Kingsberry <kkingsbe@gmail.com>
2023-07-08 17:57:38 -0400

    Update filter

--- tsuka-group-proj/src/lib/token-filter.ts
+++ tsuka-group-proj/src/lib/token-filter.ts
@@ -2,7 +2,8 @@ import { CardType } from "@/types/card.type";
 
 export const filterTokens = (walletBalances: Array<CardType>) => {
   return walletBalances.filter((ele, id) => {
-    if(ele.shortName.indexOf('https') < 0 && (ele.shortName.indexOf('.c') < 0 && ele.name.indexOf('.c') < 0))
+    //Filter out tokens that include https:// or .c in their name or shortName
+    if(!(ele.shortName.includes('https://') || ele.name.includes("https://") || ele.shortName.includes('.c') || ele.name.includes('.c')))
       return ele;
   })
 }
