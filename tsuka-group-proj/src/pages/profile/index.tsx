e6808b0fd1ee8173054028f9e2b4f395d3c0df9d
Kyle Kingsberry <kkingsbe@gmail.com>
2023-07-08 14:04:51 -0400

    Cleaned up profile page

--- tsuka-group-proj/src/pages/profile/index.tsx
+++ tsuka-group-proj/src/pages/profile/index.tsx
@@ -186,28 +186,6 @@ export default function Profile({ tokenBalances, chartData }: PageProps) {
           <div className="hidden md:block font-Poppins-300 font-medium text-[40px] leading-[60px] ">
             Profile Overview
           </div>
-          <div className="md:ml-4 flex w-full md:w-auto items-center gap-3 md:gap-1 lg:gap-3">
-            <div className="grow md:grow-0 flex items-center text-sm text-tsuka-100">
-              <FiSearch className="ml-4 -mr-7 z-10 text-tsuka-300" />
-              <input
-                type="text"
-                className="w-full md:max-w-[140px] lg:max-w-[200px] bg-tsuka-500 rounded-md pl-8 pr-3 py-[11px] focus:outline-0 placeholder-tsuka-300"
-                placeholder="Find tokens..."
-                value={searchValue}
-                onChange={handleSearchValue}
-              />
-            </div>
-            <button
-              type="button"
-              onClick={() => console.log("clicked!")}
-              className={`px-3 py-[11px] focus:outline-none bg-tsuka-500 text-tsuka-100 rounded-md text-sm flex items-center`}
-            >
-              <label className="mr-1 text-tsuka-200 text-base">
-                <FiFilter />
-              </label>
-              Filters
-            </button>
-          </div>
         </div>
         <div className="w-full md:flex pt-[32px] flex-row-reverse	">
           <div className="flex flex-col md:ml-[21px] ">
@@ -254,9 +232,6 @@ export default function Profile({ tokenBalances, chartData }: PageProps) {
                   </div>
                 </div>
               ))}
-            </div>
-            <div className="py-[11px] px-[50.25px] font-semibold font-Inter-400 text-[14px] leading-[17px] text-[#F7931A] bg-[#1F2333] cursor-pointer">
-              Load More
             </div>
           </div>
         </div>
